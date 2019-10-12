e4dcf9056f66d267eb01eb77c25365a9dc3e6815
Tom White <eugene.white@gmail.com>
2019-10-12 16:31:33 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if !logged_in?
-      redirect to '/login'
-    else
-      erb :'/tweets/index'
+      if logged_in?
+        @tweets = Tweet.all
+        erb :'tweets/tweets'
+      else
+        redirect to '/login'
+      end
     end
-  end
 
 end
